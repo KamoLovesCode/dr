89683d56d42b5383c943edf29b73e4cf500b205e
britishandbohemian <217004345@student.uj.ac.za>
2025-06-20 08:05:10 +0200

    f

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,6 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/dr-kabongo-salumu-practices' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dr-kabongo-salumu-practices' : '',
 }
 
 export default nextConfig
